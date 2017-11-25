f7e7d30ffb547a9b1bc32e60f39ec64096f69993
Mintdot <nigthmare100@naver.com>
2017-11-25 21:48:45 +0900

    인덱스 변경

---
+++ js/all.js
@@ -0,0 +1,11 @@
+var w;
+var h;
+var body;
+var back;
+
+w = window.innerWidth;
+h = window.innerHeight;
+
+body = document.getElementsByTagName("body");
+body.style.width = w;
+body.style.height = h;
