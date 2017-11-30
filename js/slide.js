2000684119b4e321f51b4031974f1b28d351b57b
wonbinlee1234 <maxtott@naver.com>
2017-11-30 22:36:38 +0900

    on 이미지슬라이드 변경

--- js/slide.js
+++ js/slide.js
@@ -1,8 +1,11 @@
-var imgs = document.getElementById("pf_wrap").children;
-var last = imgs.length-2;
+
+window.onload = function(){
+
+var imgs = document.getElementById("pf1").children;
+var last = imgs.length-1;
 var sno = 0;
-var imgs2 = document.getElementById("pf2_wrap").children;
-var last2 = imgs.length-2;
+var imgs2 = document.getElementById("pf2").children;
+var last2 = imgs2.length-1;
 var sno2 = 0;
 
 function left1(){
@@ -12,15 +15,18 @@ function left1(){
     if(sno>last) sno=0;
 
     imgs[sno].style.animationName = "slide-to-middle";
+
 }
 
 function right1(){
     imgs[sno].style.animationName = "slide-to-left";
 
-    sno++;
-    if(sno>last) sno=0;
+    sno--;
+    if(0>sno) sno=last;
 
     imgs[sno].style.animationName = "slide-to-middle2";
+
+
 }
 
 function left2(){
@@ -30,13 +36,18 @@ function left2(){
     if(sno2>last2) sno2=0;
 
     imgs2[sno2].style.animationName = "slide-to-middle";
-}
 
+}
 function right2(){
+    console.log(1);
     imgs2[sno2].style.animationName = "slide-to-left";
 
-    sno2++;
-    if(sno2>last2) sno2=0;
+    sno2--;
+    if(0>sno2) sno2=last2;
 
     imgs2[sno2].style.animationName = "slide-to-middle2";
+
+}
+    var st = setInterval(left1,1000);
+    var st2 = setInterval(left2,1000);
 }
