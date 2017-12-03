f2694ba9d735f01c7b289fb7f25de95a3f13e494
Mintdot <nigthmare100@naver.com>
2017-12-03 18:26:23 +0900

    불필요한 js 코드 제거

--- js/slide.js
+++ js/slide.js
@@ -1,12 +1,19 @@
 
 window.onload = function(){
 
-var imgs = document.getElementById("pf1").children;
-var last = imgs.length-1;
-var sno = 0;
-var imgs2 = document.getElementById("pf2").children;
-var last2 = imgs2.length-1;
-var sno2 = 0;
+var imgs;
+var last;
+var sno;
+var imgs2;
+var last2;
+var sno2;
+
+imgs = document.getElementById("pf1").children;
+last = imgs.length-1;
+sno = 0;
+imgs2 = document.getElementById("pf2").children;
+last2 = imgs2.length-1;
+sno2 = 0;
 
 function left1(){
     imgs[sno].style.animationName = "slide-to-right";
@@ -15,18 +22,6 @@ function left1(){
     if(sno>last) sno=0;
 
     imgs[sno].style.animationName = "slide-to-middle";
-
-}
-
-function right1(){
-    imgs[sno].style.animationName = "slide-to-left";
-
-    sno--;
-    if(0>sno) sno=last;
-
-    imgs[sno].style.animationName = "slide-to-middle2";
-
-
 }
 
 function left2(){
@@ -36,18 +31,8 @@ function left2(){
     if(sno2>last2) sno2=0;
 
     imgs2[sno2].style.animationName = "slide-to-middle";
-
 }
-function right2(){
-    console.log(1);
-    imgs2[sno2].style.animationName = "slide-to-left";
 
-    sno2--;
-    if(0>sno2) sno2=last2;
-
-    imgs2[sno2].style.animationName = "slide-to-middle2";
-
-}
-    var st = setInterval(left1, 2000);
-    var st2 = setInterval(left2, 2000);
+setInterval(left1, 2000);
+setInterval(left2, 2000);
 }
